c8bd48edbbe888812b79c7d37a9a9bb213ca5083
Jose Espejo <joseespejo@yahoo.com>
2024-12-02 16:45:11 +0000

    style: adjust spacing around accordion

--- client/src/components/ui/accordion.tsx
+++ client/src/components/ui/accordion.tsx
@@ -40,10 +40,10 @@ const AccordionContent = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <AccordionPrimitive.Content
     ref={ref}
-    className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm transition-all"
+    className="overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
     {...props}
   >
-    <div className={cn("pb-4 pt-0", className)}>{children}</div>
+    <div className={cn("pb-0 pt-0", className)}>{children}</div>
   </AccordionPrimitive.Content>
 ));
 
