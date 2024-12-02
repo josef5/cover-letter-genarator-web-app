00a1dd749c044e72108a392b310b72460a52788d
Jose Espejo <joseespejo@yahoo.com>
2024-12-02 21:03:36 +0000

    refactor: add isLoading and error states to form submission
    - Added `isLoading` state to track the loading status of the form submission.
    - Added `error` state to handle and display any errors that occur during the form submission process.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,10 @@ import type { UserData } from "./types/data";
 
 function App() {
   const [coverLetterText, setCoverLetterText] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
   const [userData, setUserData] = useState<UserData>({
+    // TODO: remove placeholder values
     jobDescription:
       "We are Awesome Co. and we are looking for a Software Engineer to join our team. You will be working on our core product, which is a platform that helps people write better cover letters. You will be responsible for building new features, fixing bugs, and improving the performance of our platform. The ideal candidate is passionate about writing clean code, has experience with React and Node.js, and is a great team player. If you are interested in this position, please send us your resume and a cover letter explaining why you are a good fit for this role.",
     salutation: "Dear Hiring Manager,",
@@ -19,35 +22,58 @@ function App() {
       model: "gpt-3.5-turbo",
       temperature: 0.7,
       wordLimit: 200,
-      workExperience: "",
+      workExperience:
+        "I am a frontend developer with 4 years of experience in React, Vue and TypeScript.",
     },
   });
 
   function handleSubmit(event: React.FormEvent) {
     event.preventDefault();
 
-    // console.log("userData :", userData);
+    // TODO: add validation
 
-    fetchResponse(userData);
+    // console.log("userData :", userData);
+    fetchCompletion(userData);
   }
 
-  async function fetchResponse(settings: UserData) {
+  async function fetchCompletion(settings: UserData) {
     console.log("settings:", settings);
 
-    /* const response = await fetch("http://localhost:3000/api/chat", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userData),
-    });
+    try {
+      setError(null);
+      setIsLoading(true);
 
-    const data = await response.json();
+      const response = await fetch("http://localhost:3000/api/chat", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
 
-    console.log("response: ", data.chatCompletion.choices[0].message.content);
-    setCoverLetterText(data.chatCompletion.choices[0].message.content); */
+      if (!response.ok) {
+        throw new Error(
+          `Server error. Status: ${response.status}: ${response.statusText}`,
+        );
+      }
 
-    setCoverLetterText(
+      const data = await response.json();
+
+      console.log("response: ", data.chatCompletion.choices[0].message.content);
+      setCoverLetterText(data.chatCompletion.choices[0].message.content);
+    } catch (error) {
+      if (error instanceof TypeError) {
+        console.error("Network error fetching chat completion:", error);
+      } else {
+        console.error(error);
+      }
+
+      setError((error as Error).message);
+    } finally {
+      setIsLoading(false);
+    }
+
+    /* setCoverLetterText(
       `Lorem ipsum odor amet, consectetuer adipiscing elit. Inceptos pellentesque turpis; primis pellentesque luctus in. Viverra sagittis dictum amet maecenas finibus aenean viverra semper. Parturient blandit fermentum tristique amet posuere? Phasellus lacus conubia neque auctor nisi per lectus. Eu pharetra habitant et vehicula nascetur dolor ut. Odio curabitur tempor efficitur fusce potenti. Posuere a dui venenatis gravida velit posuere. Habitasse nascetur vivamus feugiat mauris orci.
 
       Amet platea ante fusce eu auctor ornare ante. Nam class blandit dui ad orci imperdiet convallis dis mi. Enim placerat mattis inceptos purus maecenas taciti phasellus ornare. Enim sagittis eros nulla non primis. Placerat praesent mus volutpat aptent ex pharetra nullam fringilla orci. Ligula nec iaculis iaculis interdum non a eleifend. Velit magna dictum; lorem dignissim per sem. Amet egestas per donec; semper molestie curabitur. Dui sapien platea at mauris luctus mattis donec.
@@ -59,7 +85,7 @@ function App() {
       Amet platea ante fusce eu auctor ornare ante. Nam class blandit dui ad orci imperdiet convallis dis mi. Enim placerat mattis inceptos purus maecenas taciti phasellus ornare. Enim sagittis eros nulla non primis. Placerat praesent mus volutpat aptent ex pharetra nullam fringilla orci. Ligula nec iaculis iaculis interdum non a eleifend. Velit magna dictum; lorem dignissim per sem. Amet egestas per donec; semper molestie curabitur. Dui sapien platea at mauris luctus mattis donec.
 
       Netus eros velit placerat porta nullam. Habitant volutpat sed pellentesque ac rutrum metus massa. Faucibus eget nascetur elit; torquent luctus ex euismod. Senectus mus conubia tortor tempus imperdiet in donec cubilia netus. Magna arcu risus vel; aptent nec egestas? Eu suspendisse auctor quam posuere fermentum pharetra convallis. Consectetur commodo elit mi taciti tortor torquent. Erat viverra orci nisi non porttitor vivamus efficitur porttitor.`,
-    );
+    ); */
   }
 
   useEffect(() => {
@@ -102,17 +128,19 @@ function App() {
             <Button type="submit">Generate</Button>
           </div>
         </form>
-        {!coverLetterText ? (
+        {error && <div className="text-red-500">{error}</div>}
+        {isLoading ? (
           <div>Loading...</div>
-        ) : coverLetterText !== null && coverLetterText !== undefined ? (
-          <Textarea
-            style={{ whiteSpace: "pre-line" }}
-            value={coverLetterText}
-            // readOnly
-            onChange={(event) => setCoverLetterText(event.target.value)}
-            className="flex-1 bg-white text-neutral-800"
-          ></Textarea>
-        ) : null}
+        ) : (
+          coverLetterText && (
+            <Textarea
+              style={{ whiteSpace: "pre-line" }}
+              value={coverLetterText}
+              onChange={(event) => setCoverLetterText(event.target.value)}
+              className="flex-1 bg-white text-neutral-800"
+            ></Textarea>
+          )
+        )}
       </div>
     </div>
   );
