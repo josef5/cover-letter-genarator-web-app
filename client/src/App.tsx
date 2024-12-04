8befb179b141bce52edcd6470bc9b26450bd68e1
Jose Espejo <joseespejo@yahoo.com>
2024-12-04 20:31:02 +0000

    refactor: use hookform for validation and shadcn Form components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,36 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import * as z from "zod";
 import "./App.css";
 import SettingsAccordion from "./components/SettingsAccordion";
-import { Textarea } from "@/components/ui/textarea";
-import { Label } from "./components/ui/label";
-import { Input } from "./components/ui/input";
 import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
 import type { UserData } from "./types/data";
+
+const schema = z.object({
+  jobDescription: z.string().min(1, { message: "Required" }),
+  salutation: z.string().min(1, { message: "Required" }),
+  settings: z.object({
+    apiKey: z.string().min(1, { message: "Required" }),
+    name: z.string().min(1, { message: "Required" }),
+    model: z.string().min(1, { message: "Required" }),
+    temperature: z.number().min(0, { message: "Required" }),
+    wordLimit: z.number().min(1, { message: "Required" }),
+    workExperience: z.string().min(1, { message: "Required" }),
+  }),
+});
+
+type FormValues = z.infer<typeof schema>;
 
 function App() {
   const [coverLetterText, setCoverLetterText] = useState("");
@@ -27,19 +52,36 @@ function App() {
     },
   });
 
-  function handleSubmit(event: React.FormEvent) {
-    event.preventDefault();
+  const form = useForm<FormValues>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      // TODO: remove placeholder values
+      jobDescription:
+        "We are Awesome Co. and we are looking for a Software Engineer to join our team. You will be working on our core product, which is a platform that helps people write better cover letters. You will be responsible for building new features, fixing bugs, and improving the performance of our platform. The ideal candidate is passionate about writing clean code, has experience with React and Node.js, and is a great team player. If you are interested in this position, please send us your resume and a cover letter explaining why you are a good fit for this role.",
+      salutation: "Dear Hiring Manager,",
+      settings: {
+        apiKey: "abc123",
+        name: "John Doe",
+        model: "gpt-3.5-turbo",
+        temperature: 0.7,
+        wordLimit: 200,
+        workExperience:
+          "I am a frontend developer with 4 years of experience in React, Vue and TypeScript.",
+      },
+    },
+  });
 
-    // TODO: add validation
+  function onSubmit(data: FormValues) {
+    // TODO: close settings accordion here
 
     // console.log("userData :", userData);
-    fetchCompletion(userData);
+    fetchCompletion(data);
   }
 
-  async function fetchCompletion(settings: UserData) {
-    console.log("settings:", settings);
+  async function fetchCompletion(userData: UserData) {
+    console.log("userData :", userData);
 
-    try {
+    /* try {
       setError(null);
       setIsLoading(true);
 
@@ -71,9 +113,11 @@ function App() {
       setError((error as Error).message);
     } finally {
       setIsLoading(false);
-    }
+    } */
 
-    /* setCoverLetterText(
+    setIsLoading(false);
+
+    setCoverLetterText(
       `Lorem ipsum odor amet, consectetuer adipiscing elit. Inceptos pellentesque turpis; primis pellentesque luctus in. Viverra sagittis dictum amet maecenas finibus aenean viverra semper. Parturient blandit fermentum tristique amet posuere? Phasellus lacus conubia neque auctor nisi per lectus. Eu pharetra habitant et vehicula nascetur dolor ut. Odio curabitur tempor efficitur fusce potenti. Posuere a dui venenatis gravida velit posuere. Habitasse nascetur vivamus feugiat mauris orci.
 
       Amet platea ante fusce eu auctor ornare ante. Nam class blandit dui ad orci imperdiet convallis dis mi. Enim placerat mattis inceptos purus maecenas taciti phasellus ornare. Enim sagittis eros nulla non primis. Placerat praesent mus volutpat aptent ex pharetra nullam fringilla orci. Ligula nec iaculis iaculis interdum non a eleifend. Velit magna dictum; lorem dignissim per sem. Amet egestas per donec; semper molestie curabitur. Dui sapien platea at mauris luctus mattis donec.
@@ -85,7 +129,7 @@ function App() {
       Amet platea ante fusce eu auctor ornare ante. Nam class blandit dui ad orci imperdiet convallis dis mi. Enim placerat mattis inceptos purus maecenas taciti phasellus ornare. Enim sagittis eros nulla non primis. Placerat praesent mus volutpat aptent ex pharetra nullam fringilla orci. Ligula nec iaculis iaculis interdum non a eleifend. Velit magna dictum; lorem dignissim per sem. Amet egestas per donec; semper molestie curabitur. Dui sapien platea at mauris luctus mattis donec.
 
       Netus eros velit placerat porta nullam. Habitant volutpat sed pellentesque ac rutrum metus massa. Faucibus eget nascetur elit; torquent luctus ex euismod. Senectus mus conubia tortor tempus imperdiet in donec cubilia netus. Magna arcu risus vel; aptent nec egestas? Eu suspendisse auctor quam posuere fermentum pharetra convallis. Consectetur commodo elit mi taciti tortor torquent. Erat viverra orci nisi non porttitor vivamus efficitur porttitor.`,
-    ); */
+    );
   }
 
   useEffect(() => {
@@ -96,38 +140,43 @@ function App() {
     <div className="App mx-auto max-w-5xl">
       <div className="flex h-screen flex-col gap-4 py-4">
         <h1 className="text-base font-bold">Cover Letter Generator</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="flex flex-col gap-4">
-            <div className="flex flex-col gap-1">
-              <Label htmlFor="salutation" className="text-xs">
-                Salutation
-              </Label>
-              <Input id="salutation" defaultValue={userData.salutation}></Input>
-            </div>
-            <div className="flex flex-col gap-1">
-              <Label htmlFor="job-description" className="text-xs">
-                Job Description
-              </Label>
-              <Textarea
-                id="job-description"
-                value={userData.jobDescription}
-                onChange={(event) =>
-                  setUserData({
-                    ...userData,
-                    jobDescription: event.target.value,
-                  })
-                }
-              ></Textarea>
-            </div>
-            <SettingsAccordion
-              settings={userData.settings}
-              onUpdate={(newSettings) =>
-                setUserData({ ...userData, settings: newSettings })
-              }
-            />
-            <Button type="submit">Generate</Button>
-          </div>
-        </form>
+        <FormProvider {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <Form {...form}>
+              <div className="flex flex-col gap-4">
+                <FormField
+                  control={form.control}
+                  name="salutation"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col gap-1">
+                      <FormLabel className="text-xs">Salutation</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Dear Hiring Manager," {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="jobDescription"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col gap-1">
+                      <FormLabel className="text-xs">Job Description</FormLabel>
+                      <FormControl>
+                        <Textarea {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <SettingsAccordion />
+                <Button type="submit">Generate</Button>
+              </div>
+            </Form>
+          </form>
+        </FormProvider>
         {error && <div className="text-red-500">{error}</div>}
         {isLoading ? (
           <div>Loading...</div>
--- client/src/components/SettingsAccordion.tsx
+++ client/src/components/SettingsAccordion.tsx
@@ -5,6 +5,14 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import {
+  // Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
   Select,
   SelectContent,
   SelectItem,
@@ -12,42 +20,21 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { useState } from "react";
+import { useFormContext } from "react-hook-form";
 import { Input } from "./ui/input";
-import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
-import { Settings } from "@/types/data";
 
-function SettingsAccordion({
-  settings = {
-    apiKey: "",
-    name: "",
-    model: "gpt-3.5-turbo",
-    temperature: 0.7,
-    wordLimit: 200,
-    workExperience: "",
-  },
-  onUpdate,
-}: {
-  settings: Settings;
-  onUpdate: (settings: Settings) => void;
-}) {
-  const [value, setValue] = useState("default"); // Change to open/close
-
-  function handleSettingsChange(
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) {
-    const { name, value } = event.target;
-
-    onUpdate({ ...settings, [name]: value });
-  }
+function SettingsAccordion() {
+  const { control } = useFormContext();
+  const [accordionValue, setAccordionValue] = useState("default"); // Change to open/close
 
   return (
     <Accordion
       type="single"
       collapsible
       className=""
-      value={value}
-      onValueChange={setValue}
+      value={accordionValue}
+      onValueChange={setAccordionValue}
     >
       <AccordionItem value="fields" className="">
         <AccordionTrigger className="rounded-lg border bg-neutral-800 px-4 text-sm">
@@ -55,97 +42,125 @@ function SettingsAccordion({
         </AccordionTrigger>
         <AccordionContent className="px-4">
           <div className="mt-4 flex flex-col gap-4">
-            <div className="flex flex-col gap-1">
-              <Label htmlFor="api-key" className="text-xs">
-                OpenAI API Key
-              </Label>
-              <Input
-                name="apiKey"
-                id="api-key"
-                type="text"
-                defaultValue={settings.apiKey}
-                onChange={handleSettingsChange}
-                className="w-full autofill:shadow-[inset_0_0_0px_1000px_hsl(var(--background))]"
+            <FormField
+              control={control}
+              name="settings.apiKey"
+              render={({ field }) => (
+                <FormItem className="flex flex-col gap-1">
+                  <FormLabel className="text-xs">OpenAI API Key</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your API key"
+                      {...field}
+                      className="w-full autofill:shadow-[inset_0_0_0px_1000px_hsl(var(--background))]"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="row-of-inputs flex justify-between gap-4">
+              <FormField
+                control={control}
+                name="settings.name"
+                render={({ field }) => (
+                  <FormItem className="flex flex-1 flex-col gap-1">
+                    <FormLabel className="text-xs">Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="e.g. John Smith"
+                        {...field}
+                        className="w-full autofill:shadow-[inset_0_0_0px_1000px_hsl(var(--background))]"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={control}
+                name="settings.model"
+                render={({ field }) => (
+                  <FormItem className="flex flex-1 flex-col gap-1">
+                    <FormLabel className="text-xs">Model</FormLabel>
+                    <FormControl>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <SelectTrigger className="w-full">
+                          <SelectValue placeholder="Model" />
+                        </SelectTrigger>
+                        <SelectContent className="w-full">
+                          <SelectItem value="gpt-3.5-turbo">
+                            gpt-3.5-turbo
+                          </SelectItem>
+                          <SelectItem value="gpt-4o">gpt-4o</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={control}
+                name="settings.temperature"
+                render={({ field }) => (
+                  <FormItem className="flex flex-1 flex-col gap-1">
+                    <FormLabel className="text-xs">Temperature</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="number"
+                        step="0.1"
+                        placeholder="0.7"
+                        min="0.0"
+                        max="2.0"
+                        {...field}
+                        className="w-full autofill:shadow-[inset_0_0_0px_1000px_hsl(var(--background))]"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={control}
+                name="settings.wordLimit"
+                render={({ field }) => (
+                  <FormItem className="flex flex-1 flex-col gap-1">
+                    <FormLabel className="text-xs">Word Limit</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="number"
+                        placeholder="300"
+                        step="100"
+                        {...field}
+                        className="w-full autofill:shadow-[inset_0_0_0px_1000px_hsl(var(--background))]"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
             </div>
-            <div className="row flex justify-between gap-4">
-              <div className="flex w-full flex-1 flex-col gap-1">
-                <Label htmlFor="name" className="text-xs">
-                  Name
-                </Label>
-                <Input
-                  name="name"
-                  id="name"
-                  type="text"
-                  placeholder="e.g. John Doe"
-                  defaultValue={settings.name}
-                  onChange={handleSettingsChange}
-                  className="w-full autofill:shadow-[inset_0_0_0px_1000px_hsl(var(--background))]"
-                />
-              </div>
-
-              <div className="flex flex-1 flex-col gap-1">
-                <Label className="text-xs">Model</Label>
-                <Select
-                  name="model"
-                  defaultValue={settings.model}
-                  onValueChange={(value) =>
-                    handleSettingsChange({
-                      target: { name: "model", value },
-                    } as React.ChangeEvent<HTMLInputElement>)
-                  }
-                >
-                  <SelectTrigger className="w-full">
-                    <SelectValue placeholder="Model" />
-                  </SelectTrigger>
-                  <SelectContent className="w-full">
-                    <SelectItem value="gpt-3.5-turbo">gpt-3.5-turbo</SelectItem>
-                    <SelectItem value="gpt-4o">gpt-4o</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-
-              <div className="flex flex-1 flex-col gap-1">
-                <Label htmlFor="temperature" className="text-xs">
-                  Temperature
-                </Label>
-                <Input
-                  name="temperature"
-                  id="temperature"
-                  type="number"
-                  step="0.1"
-                  placeholder="0.7"
-                  defaultValue={settings.temperature}
-                  onChange={handleSettingsChange}
-                  className="w-full autofill:shadow-[inset_0_0_0px_1000px_hsl(var(--background))]"
-                />
-              </div>
-
-              <div className="flex flex-1 flex-col gap-1">
-                <Label htmlFor="word-limit" className="text-xs">
-                  Word Limit
-                </Label>
-                <Input
-                  name="wordLimit"
-                  id="word-limit"
-                  type="number"
-                  step="1"
-                  placeholder="300"
-                  defaultValue={settings.wordLimit}
-                  onChange={handleSettingsChange}
-                  className="w-full autofill:shadow-[inset_0_0_0px_1000px_hsl(var(--background))]"
-                />
-              </div>
-            </div>
             <div className="flex flex-col gap-1">
-              <Label htmlFor="work-experience" className="text-xs">
-                Work Experience
-              </Label>
-              <Textarea
-                name="workExperience"
-                id="work-experience"
-                value={settings.workExperience}
-                onChange={handleSettingsChange}
+              <FormField
+                control={control}
+                name="settings.workExperience"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col gap-1">
+                    <FormLabel className="text-xs">Work Experience</FormLabel>
+                    <FormControl>
+                      <Textarea {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
             </div>
           </div>
